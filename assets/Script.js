cc92be414326cee681aae85add3689c9d7d039ff
Dorian Birch <206dorian@gmail.com>
2022-09-20 19:17:29 -0500

    added jquery link, made a JS and html

---
+++ assets/Script.js
@@ -0,0 +1,19 @@
+var APIKey = 38728577514e54c85b8192270269130c
+
+var city = ()
+
+
+//NEED A FUNCTION FOR WHEN THE USER PUTS IN A CITY IT CALCULATES 
+//EVENT LISTENER "CLICK" WHEN THRY ENTER THE CITY
+//QUERY PARAMETERS, LOCAL STORAGE 
+
+//  an API call using just the city name or by using a combination of the city name, state code, and country code.
+
+// This section lists a number of parameters, but only the following two are required:
+
+// q: The query parameter, where we'll add the city variable.
+// appid: The application id or key, where we'll add the API key variable.
+
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
+
