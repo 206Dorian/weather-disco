d30001bcdf1c90a1b3a5960f69449e454376a1fe
Dorian Birch <206dorian@gmail.com>
2022-09-20 19:52:54 -0500

    added more color to css, linked css, linked jquery, created search bar

--- assets/Script.js
+++ assets/Script.js
@@ -2,18 +2,22 @@ var APIKey = 38728577514e54c85b8192270269130c
 
 var city = ()
 
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
 
-//NEED A FUNCTION FOR WHEN THE USER PUTS IN A CITY IT CALCULATES 
+
+
+//NEED A SEARCH BAR
+//NEED A FUNCTION FOR WHEN THE USER INPUTS A CITY IT CALCULATES BACK TO  var city= ()
 //EVENT LISTENER "CLICK" WHEN THRY ENTER THE CITY
 //QUERY PARAMETERS, LOCAL STORAGE 
 
 //  an API call using just the city name or by using a combination of the city name, state code, and country code.
 
-// This section lists a number of parameters, but only the following two are required:
 
+//**api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}**
+// **This section lists a number of parameters, but only the following two are required:
 // q: The query parameter, where we'll add the city variable.
 // appid: The application id or key, where we'll add the API key variable.
 
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
 
