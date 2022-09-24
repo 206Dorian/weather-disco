266e4ac1f474898710665ab9763c660e63b18807
Dorian Birch <206dorian@gmail.com>
2022-09-24 13:24:57 -0500

    commit before tutoring session

--- assets/Script.js
+++ assets/Script.js
@@ -1,26 +1,41 @@
 var APIKey = "38728577514e54c85b8192270269130c"
 
+var theDay = moment();
+$("#date").text(theDay.format("LLL"))
 
 
-
-function retrieve(){
+function retrieve() {
     var city = $("#searchbar").val()
     console.log(city);
+
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
+
+    //fetch request here
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+//when that button is clicked give me a new element i.e card, etc...with this new element put the info into it
+var searchHistory = $("<div>").text(city).addClass("card")
+console.log(searchHistory)
+$("body").append(searchHistory)
+
+
+//GET info and put in the card
+
+        //WHY isnt this working, when above works fine?
+    // .then(function (response) {
+    //     return response.json();
+    // });
+    // .then(function (data) {
+    //     console.log(data)
+    // });
 }
 
-
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
-
-var theDay = moment();
-$ ("#date").text(theDay.format("LLL"))
-
-
-//fetch request... 
-
-
-//NEED A FUNCTION FOR WHEN THE USER INPUTS A CITY IT CALCULATES BACK TO  var city= ()
-//EVENT LISTENER "CLICK" WHEN THRY ENTER THE CITY
-//QUERY PARAMETERS, LOCAL STORAGE 
+//NEED QUERY PARAMETERS, LOCAL STORAGE 
 
 //  an API call using just the city name or by using a combination of the city name, state code, and country code.
 
@@ -31,4 +46,5 @@ $ ("#date").text(theDay.format("LLL"))
 // appid: The application id or key, where we'll add the API key variable.
 
 $("#city-bttn").on("click", retrieve)
+//runs the retrieve function
 
